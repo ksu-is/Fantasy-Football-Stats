cc8eea8b0a480b55c7449e82eec48e2e7c359527
jaymolander <59626251+jaymolander@users.noreply.github.com>
2020-04-13 20:31:08 -0400

    Parsing html object in beautiful soup

--- fantasyscrape.py
+++ fantasyscrape.py
@@ -3,4 +3,8 @@ import requests
 import csv
 
 
-source = requests.get('https://www.sharpfootballstats.com/personnel-grouping-frequency.html').t
+source = requests.get('https://www.sharpfootballstats.com/personnel-grouping-frequency.html').text
+
+soupObject = BeautifulSoup(source, "lxml")
+print(soupObject.prettify())
+
