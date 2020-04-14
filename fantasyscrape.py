4efbe09e6bb4eee38b4fd35b36ab627ba214ff33
jaymolander <59626251+jaymolander@users.noreply.github.com>
2020-04-13 21:31:29 -0400

    Collected the data from the table

--- fantasyscrape.py
+++ fantasyscrape.py
@@ -6,5 +6,15 @@ import csv
 source = requests.get('https://www.teamrankings.com/nfl/stat/points-per-game').text
 
 soupObject = BeautifulSoup(source, "lxml")
-print(soupObject.prettify())
+#print(soupObject.prettify())
+
+soupTitle = soupObject.title
+
+table = soupObject.table
+table_rows = table.find_all('tr')
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    print(row)
 
