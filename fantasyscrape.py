3ef97a45ff84ad23d9958b85fed0cf7e44d155c3
jaymolander <59626251+jaymolander@users.noreply.github.com>
2020-04-14 15:55:54 -0400

    added a module example for the chiefs

--- fantasyscrape.py
+++ fantasyscrape.py
@@ -18,3 +18,25 @@ for tr in table_rows:
     row = [i.text for i in td]
     print(row)
 
+#module for the chiefs
+def Chiefs():
+    KansasCity = ""
+    KansasCity = table_rows[2].find('td','text-right','data-sort="29.8947"') 
+    for letter in KansasCity:
+        KansasPoints =""
+        KansasPoints += letter
+        return("Kansas City scored " + KansasPoints + " points per game last year")
+#user input 
+userInput = input("What teams offensive points per game would you like to see?")
+#if statement for the chiefs
+if userInput == 'Chiefs':
+    print(Chiefs())
+else:
+    print("not the chiefs")
+
+
+
+
+
+
+
