df213ae3a4a6d85881c7f469588d8cbe67836418
jaymolander <59626251+jaymolander@users.noreply.github.com>
2020-04-22 13:40:18 -0400

    Added rushing yards

--- fantasyscrape.py
+++ fantasyscrape.py
@@ -5,12 +5,15 @@ import csv
 
 source = requests.get('https://www.teamrankings.com/nfl/stat/points-per-game').text
 passsource = requests.get('https://www.teamrankings.com/nfl/stat/passing-yards-per-game').text
+rushsource = requests.get('https://www.teamrankings.com/nfl/stat/rushing-yards-per-game').text
 
 soupObject = BeautifulSoup(source, "lxml")
 passObject = BeautifulSoup(passsource, "lxml")
+rushObject = BeautifulSoup(rushsource, "lxml")
 
 soupTitle = soupObject.title
 passTitle = passObject.title
+rushTitle = rushObject.title
 
 table = soupObject.table
 table_rows = table.find_all('tr')
@@ -28,6 +31,9 @@ for tr in passtable_rows:
     passrow = [i.text for i in tabledata]
     #print(passrow)
 
+rushtable = rushObject.table
+rushtable_rows = rushtable.find_all('tr')
+
 def teamStat(team):
     for tr in table_rows:
         td = tr.find_all('td')
@@ -42,7 +48,14 @@ def teamStat(team):
         tabledata = tr.find_all('td')
         passrow = [i.text for i in tabledata]
         if team in passrow:
-            print(passrow[2], 'passing yards per game in 2019')            
+            print(passrow[2], 'passing yards per game in 2019')   
+    for tr in rushtable_rows:
+        tabledata = tr.find_all('td')
+        rushrow = [i.text for i in tabledata]
+        if team in rushrow:
+            print(rushrow[2], 'rushing yards per game in 2019')   
+
+             
 #user input 
 userInput = input("What teams offensive points per game would you like to see? Enter the Location of the team.")
 teamStat(userInput.title())
